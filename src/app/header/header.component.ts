11c86cfdd580d329ed3840b61389e89f440bd8ae
Kartik Sharma <kartik.sharma1607@outlook.com>
2017-10-28 17:47:17 +0530

    Added header

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+  items: string[];
+  logo: string;
+
+  constructor() {
+    this.logo = '../../assets/logo.png'
+    this.items = ['About Us', 'Courses', 'Bootcamps', 'Events', 'Resources', 'Competitions', 'Contact Us'];
+  }
+
+  ngOnInit() {
+  }
+
+}
